a198282b44d144d5624183bf7783a458870f1fbd
dustinface <35775977+xdustinface@users.noreply.github.com>
2020-12-15 00:26:30 +0100

    refactor: Add/Use byte vector constructor for CBLSWrapper (#3868)
    * bls: Add CBLSWrapper constructor which accepts a byte vecor
    
    * bls: Bring CBLSWrapper::CBLSWrapper in scope of CBLSId, CBLSSecretKey, CBLSPublicKey
    
    Allow them to access e.g. the new byte vector consructor.
    
    * governance|init|privatesend|test: Refactor some BLS instantiations

--- src/init.cpp
+++ src/init.cpp
@@ -2096,8 +2096,7 @@ bool AppInitMain()
     std::string strMasterNodeBLSPrivKey = gArgs.GetArg("-masternodeblsprivkey", "");
     if (!strMasterNodeBLSPrivKey.empty()) {
         auto binKey = ParseHex(strMasterNodeBLSPrivKey);
-        CBLSSecretKey keyOperator;
-        keyOperator.SetBuf(binKey);
+        CBLSSecretKey keyOperator(binKey);
         if (!keyOperator.IsValid()) {
             return InitError(_("Invalid masternodeblsprivkey. Please see documentation."));
         }
