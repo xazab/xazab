e99230cffb9fb22fafb3a9683128294a225913f9
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-12-15 05:15:09 +0300

    rpc: Add `masternode payments` (#3863)
    * rpc: Implement `masternode payments`
    
    Returns an array of deterministic masternodes and their payments for a specific block
    
    * tests: Add rpc_masternode.py
    
    * Apply review suggestions
    
    * Add amounts calculated per masternode and per block
    
    * Tweak help string
    
    * Update src/rpc/masternode.cpp
    
    Co-authored-by: dustinface <35775977+xdustinface@users.noreply.github.com>
    
    * rpc: Check against vector size instead of decrementing a counter
    
    * rpc: Use `std::vector::begin()` instead of `std::begin(std::vector)`
    
    * Drop set_xazab_dip8_activation in rpc_masternode.py
    
    * Apply suggestions from code review
    
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
    
    Co-authored-by: dustinface <35775977+xdustinface@users.noreply.github.com>
    Co-authored-by: xdustinface <xdustinfacex@gmail.com>
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -147,6 +147,7 @@ BASE_SCRIPTS= [
     'p2p_unrequested_blocks.py', # NOTE: needs xazab_hash to pass
     'feature_shutdown.py',
     'rpc_privatesend.py',
+    'rpc_masternode.py',
     'p2p_fingerprint.py',
     'rpc_platform_filter.py',
     'feature_uacomment.py',
