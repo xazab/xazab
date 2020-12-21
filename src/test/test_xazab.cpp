5bf73835196ef7a371ae5b007479a49e632bbb18
xazab <71531505+xazab@users.noreply.github.com>
2020-12-21 08:10:05 +0100

    xazabify

--- src/test/test_xazab.cpp
+++ src/test/test_xazab.cpp
@@ -51,7 +51,7 @@ extern bool fPrintToConsole;
 extern void noui_connect();
 
 BasicTestingSetup::BasicTestingSetup(const std::string& chainName)
-    : m_path_root(fs::temp_directory_path() / "test_dash" / strprintf("%lu_%i", (unsigned long)GetTime(), (int)(InsecureRandRange(1 << 30))))
+    : m_path_root(fs::temp_directory_path() / "test_xazab" / strprintf("%lu_%i", (unsigned long)GetTime(), (int)(InsecureRandRange(1 << 30))))
 {
     SHA256AutoDetect();
     RandomInit();
