c84a3e12fa2aefee0d9430c5bdc2259fa5ea89ef
xazab <71531505+xazab@users.noreply.github.com>
2021-05-07 03:14:11 +0000

    Remove v0.17

--- INSTALL.md
+++
@@ -1,5 +0,0 @@
-Building Xazab
-=============
-
-See doc/build-*.md for instructions on building the various
-elements of the Xazab Core reference implementation of Xazab.
--- contrib/auto_gdb/xazab_dbg.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# use testnet settings,  if you need mainnet,  use ~/.xazabcore/xazabd.pid file instead
-export LC_ALL=C
-
-xazab_pid=$(<~/.xazabcore/testnet3/xazabd.pid)
-sudo gdb -batch -ex "source debug.gdb" xazabd ${xazab_pid}
